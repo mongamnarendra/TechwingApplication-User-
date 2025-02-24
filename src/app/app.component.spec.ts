398c49e67a02cd69a308a7f9a8bec68c64bfc9af
narendramongam <131798142+mongamnarendra@users.noreply.github.com>
2025-02-24 14:31:37 +0530

    User Dashboard

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AdminDashboard'`, () => {
+  it(`should have as title 'Student'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AdminDashboard');
+    expect(app.title).toEqual('Student');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AdminDashboard app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Student app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminComponent } from './Dashboard/admin/admin.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { StudentdetailsComponent } from './studentdetails/studentdetails.component';
 import {HttpClientModule} from '@angular/common/http';
-import { EditstudentComponent } from './editstudent/editstudent.component';
-import { AddstudentComponent } from './addstudent/addstudent.component'
-import { ReactiveFormsModule } from '@angular/forms';
-import { AttendanceComponent } from './attendance/attendance.component';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { FormsModule } from '@angular/forms';
-import { filter } from 'rxjs';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent,
-    EditstudentComponent,
-    AddstudentComponent,
-    AttendanceComponent,
+    WelcomePageComponent,
+    StudentdetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule,
-    FormsModule,
+    NgCircleProgressModule.forRoot({
+      "radius": 60,
+      "outerStrokeWidth": 10,
+      "innerStrokeWidth": 5,
+      "showBackground": false,
+      "startFromZero": false})
   ],
   providers: [],
   bootstrap: [AppComponent]
