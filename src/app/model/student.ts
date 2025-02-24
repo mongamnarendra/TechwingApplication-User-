ebdab16a7b53a531e40c1dc83bff607b7a0e59a1
narendramongam <131798142+mongamnarendra@users.noreply.github.com>
2025-02-24 14:34:03 +0530

    Admin Dashboard

---
+++ src/app/model/student.ts
@@ -0,0 +1,13 @@
+export class Student {
+    id!:number;
+    name?:string;
+    rollNumber?:string;
+    email!:string;
+    branch!:string;
+    programName!:string;
+    mockinterview!:string;
+    hrInterview!:string;
+    leetcodeurl!:string;
+    github!:string;
+    image!:string;
+}
